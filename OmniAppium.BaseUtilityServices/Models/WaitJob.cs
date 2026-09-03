48cfda96309201cb1893ba225ba647f33c67ea98
jayhuang820 <jay.huang@solventosoft.com.tw>
2026-09-03 15:37:30 +0800

    fix #1: fix the Job model and its JobHandler

--- OmniAppium.BaseUtilityServices/Models/WaitJob.cs
+++ OmniAppium.BaseUtilityServices/Models/WaitJob.cs
@@ -2,6 +2,5 @@ namespace OmniAppium.ConfigUtilityService.Models
 {
     public class WaitJob : Job
     {
-        public int WaitMilliSecond { get; set; }
     }
 }
--- OmniAppium.EngineUtilityServices/Utilities/WaitJobHandler.cs
+++ OmniAppium.EngineUtilityServices/Utilities/WaitJobHandler.cs
@@ -1,17 +1,47 @@
 ﻿using OmniAppium.ConfigUtilityService.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace OmniAppium.EngineUtilityService.Utilities
+namespace OmniAppium.EngineUtilityService.Utilities;
+
+/// <summary>
+/// Handles wait jobs.
+/// </summary>
+public sealed class WaitJobHandler(
+    IWaitService waitService
+) : IJobHandler
 {
-    public class WaitJobHandler(IWaitService waitService) : IJobHandler
+    private readonly IWaitService _waitService =
+        waitService ?? throw new ArgumentNullException(nameof(waitService));
+
+    /// <summary>
+    /// Determines whether this handler supports the specified job.
+    /// </summary>
+    public bool CanHandle(Job job)
     {
-        public bool CanHandle(Job job) => job is WaitJob;
-        public Task AutoExecuteAsync(Job job)
+        ArgumentNullException.ThrowIfNull(job);
+
+        return job is WaitJob;
+    }
+
+    /// <summary>
+    /// Executes the specified wait job.
+    /// </summary>
+    public Task AutoExecuteAsync(Job job)
+    {
+        ArgumentNullException.ThrowIfNull(job);
+
+        if (job is not WaitJob waitJob)
         {
-            waitService.Wait(((WaitJob)job).WaitMilliSecond);
-            return Task.CompletedTask;
+            throw new ArgumentException(
+                $"Expected {nameof(WaitJob)}, but received {job.GetType().Name}.",
+                nameof(job));
         }
+
+        ArgumentOutOfRangeException.ThrowIfNegative(
+            waitJob.Timeout,
+            nameof(waitJob.Timeout));
+
+        _waitService.Wait(waitJob.Timeout);
+
+        return Task.CompletedTask;
     }
 }
